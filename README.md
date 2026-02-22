d19e896ddd1ec672927075b6edd0d296f45facff
Kobayasi, Hiroaki <buribullet@gmail.com>
2026-02-22 11:43:05 +0900

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+CGI::PSGI::Minimum - It's new $module
+
+# SYNOPSIS
+
+    use CGI::PSGI::Minimum;
+
+# DESCRIPTION
+
+CGI::PSGI::Minimum is ...
+
+# LICENSE
+
+Copyright (C) Kobayasi, Hiroaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kobayasi, Hiroaki <buribullet@gmail.com>
---
+++ lib/CGI/PSGI/Minimum.pm
@@ -0,0 +1,39 @@
+package CGI::PSGI::Minimum;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+CGI::PSGI::Minimum - It's new $module
+
+=head1 SYNOPSIS
+
+    use CGI::PSGI::Minimum;
+
+=head1 DESCRIPTION
+
+CGI::PSGI::Minimum is ...
+
+=head1 LICENSE
+
+Copyright (C) Kobayasi, Hiroaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kobayasi, Hiroaki E<lt>buribullet@gmail.comE<gt>
+
+=cut
+
